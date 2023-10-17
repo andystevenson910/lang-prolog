3d449f72c03d36bdfbb665a89b0472f0af2c8fce
Andrew Stevenson <andystevenson910@gmail.com>
2023-10-17 13:30:28 -0600

    first commit

---
+++ castle.pl
@@ -0,0 +1,10 @@
+% Base case, empty list, do nothing
+printList([]).
+
+% Add your comments here
+solveRooms(Castle, L) :- 
+  printList(NL).
+
+% Add your comments here
+solveRoomsWithinCost(Castle, Limit) :- 
+  printList(NL).
